ed5d2f7b6e7a878f8a9230f9b19249345d00e4e1
Ivan Dimitrov <ivanthechemist@gmail.com>
2023-08-03 15:42:51 +0300

    Add two course tests

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -17,8 +17,16 @@ describe("home page", () => {
 
   context("Courses section", () => {
     it("Course: Testing Your First Next.js Application", () => {
-        cy.getByData("course-0").find("a").contains("Get started").click()
-        cy.location("pathname").should("equal", "/testing-your-first-application")
+      cy.getByData("course-0").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/testing-your-first-application")
+    })
+    it("Course: Testing Foundations", () => {
+      cy.getByData("course-1").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/testing-foundations")
+    })
+    it("Course: Cypress Fundamentals", () => {
+      cy.getByData("course-2").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/cypress-fundamentals")
     })
   })
 })
